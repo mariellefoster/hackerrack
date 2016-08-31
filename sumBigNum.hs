05fb25bf9fbf21e01119398be4cef975af17de7f
Marielle Foster <marielleefoster@gmail.com>
2016-08-31 13:08:10 -0400

    haskell things

---
+++ sumBigNum.hs
@@ -0,0 +1,22 @@
+import Control.Applicative
+import Control.Monad
+import System.IO
+
+
+main :: IO ()
+main = do
+    n_temp <- getLine
+    let n = read n_temp :: Int
+    arr_temp <- getLine
+    let arr = map read $ words arr_temp :: [Integer]
+    putStrLn $ show $ sum arr
+
+getMultipleLines :: Int -> IO [String]
+
+getMultipleLines n
+    | n <= 0 = return []
+    | otherwise = do          
+        x <- getLine         
+        xs <- getMultipleLines (n-1)    
+        let ret = (x:xs)    
+        return ret      
